f3e8d11f05ddb0d988aa69b62cb5c6be2610909a
Marek Wasyluk <marekwasyluk@wp.pl>
2023-02-23 00:58:13 +0100

    ref: add space between tags values

--- src/pages/Tests/_components/TestHeader.js
+++ src/pages/Tests/_components/TestHeader.js
@@ -8,13 +8,19 @@ export default function TestHeader(props) {
         passPercentage,
     } = props;
 
+    const tagsString = tags.map((t, i) => {
+        if (i < 1)
+            return t;
+        return ' ' + t;
+    })
+
     return (
         <div>
             <h1 className="view-title">{name}</h1>
             <div className='view-subtitle'>
                 <div className='subtitle-tiles'>
                     <div>{'Typ: ' + type}</div>
-                    <div>{'Tagi: ' + tags}</div>
+                    <div>{'Tagi: ' + tagsString}</div>
                     <div>{'Zalicza: ' + passPercentage + '%'}</div>
                 </div>
             </div>
